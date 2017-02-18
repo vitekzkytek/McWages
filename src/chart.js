3c0e04b601aaeebca15607f4ad0b4a8c55e477f6
vitekzkytek <vitekzkytek@gmail.com>
2017-02-18 02:10:30 +0100

    redesign fix, color schemes

--- src/chart.js
+++ src/chart.js
@@ -1,25 +1,111 @@
-//var data= data;
+//TODO 
+//    - Tooltip - bud 2 moznosti nebo se pro jednu rozhodnout + v Evrope se zobrazuje velmi nizko
+//    - Gradient - uplne celej, vcetne tri konkretnich navrhu
+//    - Dokoncit Zoom menu - stabilni pozice hlavne dukladne otestovat
+//    - ZoomOut hover nefunguje
+//    - TextBox pod mapou - pohrat si s malymi burgry neco neco takoveho? 
+//    - nefunguji jine promenne nez BMPH
+//    - zpresni zoomovani
+//    - opravit panning a zooming po zoomu s tlacitkem
+
+
+//MAIN DEFINITIONS
 var year = 2016;
 var varName = 'BMPH';
-var variantSelect = 'bins';
-var domRange = [[0,0.20],[0.20,0.40],[0.40,0.60],[0.60,0.80],[0.80,1]];
-var colRange = ['#69D2E7','#A7DBD8','#E0E4CC','#F38630','#FA6900'];
+var variantSelect = '"rete';
+var colorSelect = 'GreenOrange';
+var domRanges =  {
+	"BMPH": [[0,0.20],[0.20,0.40],[0.40,0.60],[0.60,0.80],[0.80,1]],
+	"McWages":[[0,20],[20,40],[40,60],[60,80],[80,100]],
+	"McWages_PPP":[[0,20],[20,40],[40,60],[60,80],[80,100]],
+	"BigMacPrice": [[0,20],[20,40],[40,60],[60,80],[80,100]]
+}
+//var colRange = ['#BBBB88','#CCC68D','#EEDD99','#EEC290','#EEAA88'];//A7DBD8 96ddeb f38630
 var legendText = ['< 0.2', '0.2 - 0.4','0.4 - 0.6','0.6 - 0.8','> 0.8'];
-var transitionCols = ['rgba(105,210,231,1)','rgba(250,105,0,1)'];
-var max = getMax(data.Countries,year,varName);
-var min = getMin(data.Countries,year,varName);
+var transitionCols = ['#69D2E7','#FA6900'];
+var max;	
+var min;
+var discretes = {
+		"GreenOrange" : ['#75995A',"#AAC92D","#E0D819","#EEBB11","#FD9B08"],
+		"BlueOrange" : ["#69D2E7","#9fe7f5","#E0E4CC","#F38630","#FA6900"],
+		"LightGreenOrange" : ["#BBBB88","#CCC68D","#EEDD99","#EEC290","#EEAA88"]
+}
+
+var gradients = {
+		
+}
 
 
+
+//CONTROLLING VARIANTS AND VARIABLES
+
+function changeVariant(variant,colors,id)
+{
+	    $(".credit a").removeClass("activeVariant");
+	    $('#' + id).addClass("activeVariant");   
+
+
+	colorSelect = colors
+	variantSelect = variant;
+	LoadMap();
+};
+
+function changeYear(xYear)
+{
+	year = xYear;
+	$('#btnYear').html(xYear + '  <img src="src/down-arrow.png" class="menu-icon" />');
+	LoadMap();
+}
+
+function changeVar(xVar)
+{
+	varName = xVar;
+	$('#btnVar').html(data.seriesDetails[xVar].desc + '  <img src="src/down-arrow.png" class="menu-icon" />' );
+	LoadMap();
+}
+
+//MAIN LOADING FUNCTION
+function LoadMap(version)
+{
+	max = getMax(data.Countries,year,varName);
+	min = getMin(data.Countries,year,varName);
+	var zoom = d3.zoom()
+	.scaleExtent([1, 5])
+	.on("zoom", zoomed);
+	
+	var svg = d3.select("#gContainer").call(zoom)
+
+	$('.ActiveState').removeClass('ActiveState');
+
+	
+	$.each(data.Countries,function(d)
+		{
+						val = this[year][varName]
+			if(val != 'NA')
+				{
+					$('#' +d).css('fill',getColorFromValue(val));					
+					$('#' + d).addClass('ActiveState')
+
+				}
+			else
+				{
+					$('#' + d).css('fill','#AAAAAA')
+				}
+		}
+    );
+	handleEvents();
+	drawLegend();
+};
+//HELPING FUNCTIONS - COLORS
 function getColorFromValue(val,dom)
 {
 	var colorScale;
 	
 	if (variantSelect == 'gradient') {
-		colorScale = d3.scaleLinear().domain([min,max]).range(transitionCols);
+		colorScale = d3.scaleLinear().domain([min,max]).range(transitionCols).interpolate(d3.interpolateHcl);
 		return colorScale(val);
 	}
 	else {
-
 		return ValToCol(val);
 	}
 	
@@ -30,15 +116,16 @@ function ValToCol(val)
 	var result = '';
 	for (i =0; i < 5; i++)
 		{
-			if((val >= domRange[i][0]) && (val < domRange[i][1]))
+			if((val >= domRanges[varName][i][0]) && (val < domRanges[varName][i][1]))
 				{
-					result = colRange[i];
+					result = discretes[colorSelect][i];
 					break;
 				}
 		}
 	return result;	
 };
 
+//MAX-MIN
 function getMax(countries, year,varName) {
     var max;
     for (var country in countries)    {
@@ -57,55 +144,6 @@ function getMin(countries, year,varName) {
     return min;
 };
 
-function changeVariant(variant)
-{
-
-		$('#gradient-link').toggleClass('activeVariant')
-		$('#discrete-link').toggleClass('activeVariant')
-	variantSelect = variant;
-	LoadMap();
-}
-
-function changeYear(xYear)
-{
-	year = xYear;
-	$('#btnYear').text(xYear);
-	LoadMap();
-}
-
-function changeVar(xVar)
-{
-	varName = xVar;
-	$('#btnVar').text(data.seriesDetails[xVar].desc);
-	LoadMap();
-}
-
-function LoadMap(version)
-{
-	var zoom = d3.zoom()
-	.scaleExtent([1, 5])
-	.on("zoom", zoomed);
-	
-	var svg = d3.select("#gContainer").call(zoom)
-
-
-	
-	$.each(data.Countries,function(d)
-		{
-			val = this[year][varName]
-			if(val != 'NA')
-				{
-					$('#' +d).css('fill',getColorFromValue(val));
-				}
-			else
-				{
-					$('#' + d).css('fill','#CCCCCC')
-				}
-		}
-    );
-	handleEvents();
-	drawLegend();
-};
 
 function drawLegend()
 {
@@ -126,7 +164,7 @@ function drawLegend()
 		}
 	else 
 		{
-			colors = colRange;
+			colors = discretes[colorSelect];
 			texts = legendText
 		}
 	
@@ -134,7 +172,7 @@ function drawLegend()
 		  				.data(colors)
 		  				.enter().append("g")
 		  				.attr("class", "legend")
-		  				.attr("transform", function(d, i) { return "translate(" + i * 90 + ",5)"; });
+		  				.attr("transform", function(d, i) { return "translate(" + i * 100 + ",5)"; });
 		
 		  legend.append("rect")
 			      .attr("x", 180)
@@ -161,18 +199,18 @@ function TransformZoom(s,ZoomName){
 	if (ZoomName != 'Zoom Out')
 		{
 			$('#zoomBtn').text(ZoomName);
-			$('#zoomOutBtn').css('display','inline-block');
+			$('#divZoomOut').css('display','inline-block');
 		}
 	else 
 		{
 			$('#zoomBtn').text('Zoom to')
-			$('#zoomOutBtn').css('display','none')
+			$('#divZoomOut').css('display','none')
 		}
 };
 
 function handleEvents()
 {
-	var tooltip = d3.select("body").append("div")
+	var tooltip = d3.select("#tltp").append("div")
 	.attr("class","tooltip")
 	.style("opacity",0);
 	
@@ -189,13 +227,8 @@ function handleEvents()
 			tooltip.transition()
 			.duration(500)
 			.style('opacity',0);
-			}})
-		.on('mousemove',function(){
-			if (data.Countries.hasOwnProperty(this.id)){			
-			tooltip
-				.style('left', d3.event.pageX + 'px')
-				.style('top',(d3.event.pageY - 28) + 'px')
 			}});
+
 };
 
 function getTooltipText(el){
